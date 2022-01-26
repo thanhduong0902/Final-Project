762b3c781ccf56367670c9dd32df02984584dc3a
unknown <thanhpot88@gmail.com>
2022-01-26 20:54:44 +0700

    d

--- project/script.js
+++ project/script.js
@@ -41,78 +41,36 @@ function thanhToan() {
 }
 
 /* thanh toan */
+function objProduct(Img, Name, Price) {
+    var product = new Object();
+    product.Img = Img;
+    product.Name = Name;
+    product.Price = Price;
 
+    product.toJson = function () {
+        var json = JSON.stringify(this);
+        return json;
+    }
+    return product;
+}
 const btn = document.querySelectorAll(".order")
 //console.log(btn);
+var listSP = localStorage.getItem('listSP');
+if (listSP == null) {
+    listSP = new Array();
+}
 
 btn.forEach(function (button, index) {
     button.addEventListener("click", function (event) {
         var btnItem = event.target
-        var product1 = btnItem.parentElement
-        var product = product1.parentElement
+        var product = btnItem.parentElement.parentElement
         var productImg = product.querySelector("img").src
         var productName = product.querySelector("H3").innerText
         var productPrice = product.querySelector("p").innerText
-        addcart(productPrice, productImg, productName)
+        var meal = objProduct(productImg, productName, productPrice);
+        listSP.push(meal);
+        var jsonSP = JSON.stringify(listSP);
+        localStorage.setItem('listSP', jsonSP);
     })
 })
 
-function addcart(productPrice, productImg, productName) {
-    var addtr = document.createElement("tr")
-    var cartItem = document.querySelectorAll("tbody tr")
-    var trcontent = '<tr><td style="display: flex; align - items: center; "><img style="width: 70px" src="' + productImg + '" alt=""><span class="title">' + productName + '</span></td><td><p><span class="prices">' + productPrice + '</span><sup>đ</sup></p></td><td><input style="width: 30px; outline: none; " type="number" value="1" min="1"></td><td style="cursor: pointer; "><span class="delete">Xóa</span></td></tr>'
-    addtr.innerHTML = trcontent
-    var cartTable = document.querySelector("tbody")
-
-    cartTable.append(addtr)
-    carttotal()
-    deleteCart()
-
-
-}
-//---------------------totalprice----------------------------
-function carttotal() {
-    var cartItem = document.querySelectorAll("tbody tr")
-    var totalC = 0
-    for (var i = 0; i < cartItem.length; i++) {
-        var inputValue = cartItem[i].querySelector("input").value
-        // console.log(inputValue)
-        var productPrice = cartItem[i].querySelector(".prices").innerHTML
-        //console.log(productPrice)
-        totalA = inputValue * productPrice * 1000
-
-        totalC = totalC + totalA
-        totalD = totalC.toLocaleString('de-DE')
-        console.log(totalD)
-
-    }
-    var cartTotalA = document.querySelector(".price-total span")
-    cartTotalA.innerHTML = totalD
-
-    inputchange()
-
-}
-//---------------------Deletet cart----------------------------
-function deleteCart() {
-    var cartItem = document.querySelectorAll("tbody tr")
-    for (var i = 0; i < cartItem.length; i++) {
-        var productT = document.querySelectorAll(".delete")
-        productT[i].addEventListener("click", function (event) {
-            var cartDelete = event.target
-            var cartitemR = cartDelete.parentElement.parentElement
-            cartitemR.remove()
-            carttotal()
-            // console.log(cartitemR)
-        })
-    }
-}
-function inputchange() {
-    var cartItem = document.querySelectorAll("tbody tr")
-    for (var i = 0; i < cartItem.length; i++) {
-        var inputValue = cartItem[i].querySelector("input")
-        inputValue.addEventListener("change", function () {
-            carttotal()
-        })
-
-    }
-}
