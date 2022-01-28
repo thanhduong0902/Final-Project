557716d4db1d7ad07b19ec6af1c2e91fea5e2a50
NgocBaoTa <96873510+NgocBaoTa@users.noreply.github.com>
2022-01-28 17:27:22 +0700

    Update script.js

--- project/script.js
+++ project/script.js
@@ -33,7 +33,9 @@ menuTabs.addEventListener("click", function (e) {
 })
 
 function Redirect() {
-    window.location = "http://127.0.0.1:5500/img/formLogin.html?#";
+    loginForm.style.display = "inline-block";
+    document.body.style.backgroundColor = "black";
+    document.body.style.opacity = "0.8"
 }
 
 function thanhToan() {
@@ -74,3 +76,244 @@ btn.forEach(function (button, index) {
     })
 })
 
+
+//--------------------------------Login Form-------------------------------------------///
+
+const loginForm = document.getElementById("loginForm")
+const btnLogin = document.querySelector(".btnLogin")
+const btnRegister = document.querySelector(".btnRegister")
+const inputLogin = document.querySelector(".inputLogin")
+const inputRegister = document.querySelector(".inputRegister")
+const emailLogin = document.querySelector(".emailLogin")
+const passLogin = document.querySelector(".passLogin")
+const emailRgt = document.querySelector(".emailRgt")
+const passRgt = document.querySelector(".passRgt")
+const passRgtConfirm = document.querySelector(".passRgtConfirm")
+const yourName = document.querySelector(".yourName")
+const errorMess = document.getElementById("error")
+const submitLogin = document.querySelector(".submitLogin")
+const submitRgt = document.querySelector(".submitRgt")
+const close = document.querySelector(".close")
+
+
+close.onclick = (e) => {
+    e.preventDefault();
+    loginForm.style.display = "none";
+    document.body.style.backgroundColor = "initial";
+    document.body.style.opacity = "initial";
+    rightInputs(passLogin);
+    rightInputs(yourName);
+    rightInputs(emailRgt);
+    rightInputs(passRgt);
+    rightInputs(passRgtConfirm);
+    rightInputs(emailLogin);
+    removeInputValue(passLogin);
+    removeInputValue(yourName);
+    removeInputValue(emailRgt);
+    removeInputValue(passRgt);
+    removeInputValue(passRgtConfirm);
+    removeInputValue(emailLogin);
+}
+
+function removeInputValue(input) {
+    if (input.value.trim() !== "") {
+        input.value = ""
+    }
+}
+
+btnLogin.onclick = ((e) => {
+    e.preventDefault();
+    inputLogin.style.display = "block";
+    inputRegister.style.display = "none";
+    btnLogin.style.backgroundColor = "#f36060";
+    btnRegister.style.backgroundColor = "#f3f1ef";
+    
+})
+
+btnRegister.onclick = ((e) => {
+    e.preventDefault();
+    inputRegister.style.display = "block";
+    inputLogin.style.display = "none";
+    btnRegister.style.backgroundColor = "#f36060"; 
+    btnLogin.style.backgroundColor = "#f3f1ef";
+})
+
+inputLogin.onsubmit = function(e) {
+    e.preventDefault();
+    checkLoginInputs();
+}
+
+inputRegister.onsubmit = (e) => {
+    e.preventDefault();
+    checkRgtInputs();
+}
+
+function setError(input, message) {
+    const errorMess = input.parentNode.querySelector("#error");
+    errorMess.innerHTML = message;
+}
+
+function isEmail(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+}
+
+function setBlur(input) {
+    input.style.border = "2px solid rgb(252, 77, 77)"
+    input.style.borderRadius = "5px";
+}
+
+function removeErrorMess(input) {
+    const removeErrorMess = input.parentNode.querySelector("#error");
+    removeErrorMess.innerHTML = "";
+    input.style.border = "2px solid black";
+    input.style.borderRadius = "5px";
+}
+
+function rightInputs(input) {
+    const rightInputs = input.parentNode.querySelector("#error");
+    rightInputs.innerHTML = "";
+    input.style.border = "2px solid black";
+    input.style.borderRadius = "5px";
+}
+
+// Check Email Login
+emailLogin.onblur = (e) => {
+    if(emailLogin.value.trim() === "") {
+        setError(emailLogin, "Please enter your Email!")
+        setBlur(emailLogin)
+    } else if(!isEmail(emailLogin.value.trim())) {
+        setError(emailLogin, "Email is not valid")
+        setBlur(emailLogin)
+    } 
+}
+
+emailLogin.oninput = (e) => {
+    removeErrorMess(emailLogin)
+}
+
+//Check Password Login
+passLogin.onblur = (e) => {
+    if(passLogin.value.trim() === "") {
+        setError(passLogin, "Please enter your Password!")
+        setBlur(passLogin)
+    } else if(passLogin.value.trim().length <= 7) {
+        setError(passLogin, "Password must be longer than 7 characters")
+        setBlur(passLogin)
+    } 
+}
+
+passLogin.oninput = (e) => {
+    removeErrorMess(passLogin);
+}
+
+//Check Login submit
+function checkLoginInputs() {
+    if(emailLogin.value.trim() === "") {
+        setError(emailLogin, "Please enter your Email!")
+        setBlur(emailLogin)
+    } else if(!isEmail(emailLogin.value.trim())) {
+        setError(emailLogin, "Email is not valid")
+        setBlur(emailLogin)
+    } else rightInputs(emailLogin)
+
+    if(passLogin.value.trim() === "") {
+        setError(passLogin, "Please enter your Password!")
+        setBlur(passLogin)
+    } else if(passLogin.value.trim().length <= 7) {
+        setError(passLogin, "Password must be longer than 7 characters")
+        setBlur(passLogin)
+    } else rightInputs(passLogin)
+}
+
+
+// ---------------------------------- //
+
+
+//Check Name Register
+yourName.onblur = (e) => {
+    if(yourName.value.trim() === "") {
+        setError(yourName, "Please enter your Name!")
+        setBlur(yourName)
+    } 
+}
+
+yourName.oninput = (e) => {
+    removeErrorMess(yourName)
+}
+
+// Check Email Register
+emailRgt.onblur = (e) => {
+    if(emailRgt.value.trim() === "") {
+        setError(emailRgt, "Please enter your Email!")
+        setBlur(emailRgt)
+    } else if(!isEmail(emailRgt.value.trim())) {
+        setError(emailRgt, "Email is not valid")
+        setBlur(emailRgt)
+    } 
+}
+
+emailRgt.oninput = (e) => {
+    removeErrorMess(emailRgt)
+}
+
+// Check Password Register
+passRgt.onblur = (e) => {
+    if(passRgt.value.trim() === "") {
+        setError(passRgt, "Please enter your Password!")
+        setBlur(passRgt)
+    } else if(passRgt.value.trim().length <= 7) {
+        setError(passRgt, "At least 7 characters!")
+        setBlur(passRgt)
+    } 
+}
+
+passRgt.oninput = (e) => {
+    removeErrorMess(passRgt)
+}
+
+// Check Password Register Confirm
+passRgtConfirm.onblur = (e) => {
+    if(passRgtConfirm.value.trim() === "") {
+        setError(passRgtConfirm, "Please enter your Password Confirm!")
+        setBlur(passRgtConfirm)
+    } else if(passRgtConfirm.value.trim() !== passRgt.value.trim()) {
+        setError(passRgtConfirm, "Passwords do not match!")
+        setBlur(passRgtConfirm)
+    } 
+}
+
+passRgtConfirm.oninput = (e) => {
+    removeErrorMess(passRgtConfirm)
+}
+
+//Check Register submit
+function checkRgtInputs() {
+    if(yourName.value.trim() === "") {
+        setError(yourName, "Please enter your Name!")
+        setBlur(yourName)
+    } else rightInputs(yourName)
+
+    if(emailRgt.value.trim() === "") {
+        setError(emailRgt, "Please enter your Email!")
+        setBlur(emailRgt)
+    } else if(!isEmail(emailRgt.value.trim())) {
+        setError(emailRgt, "Email is not valid")
+        setBlur(emailRgt)
+    } else rightInputs(emailRgt)
+
+    if(passRgt.value.trim() === "") {
+        setError(passRgt, "Please enter your Password!")
+        setBlur(passRgt)
+    } else if(passRgt.value.trim().length <= 7) {
+        setError(passRgt, "At least 7 characters!")
+        setBlur(passRgt)
+    } else rightInputs(passRgt)
+
+    if(passRgtConfirm.value.trim() === "") {
+        setError(passRgtConfirm, "Please enter Password Confirm!")
+        setBlur(passRgtConfirm)
+    } else if(passRgtConfirm.value.trim() !== passRgt.value.trim()) {
+        setError(passRgtConfirm, "Passwords do not match!")
+        setBlur(passRgtConfirm)
+    } else rightInputs(passRgtConfirm)
+}
